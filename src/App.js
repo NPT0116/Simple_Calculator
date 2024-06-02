50284e3e72d51243d210ac441ee535a3346531c1
NPT0116 <thanh1612004@gmail.com>
2024-06-02 23:35:52 +0700

    add full package first time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext,useState } from "react";
+import { InputCalculatorContext } from "./context";
+import './App.css'
 
 function App() {
+  const context = useContext(InputCalculatorContext)
+  var [result, setResult] = useState(0);
+  function inputData(e)
+  {
+    const inputValue = e.target.value;
+    
+ 
+    context.setData(inputValue);
+  
+    console.log ("input", inputValue)
+    console.log(context.data);
+  }
+  function doAdd(e)
+  {
+    setResult(prevResult => prevResult + parseInt(context.data))
+  }
   return (
+  
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simplest Working Calculator</h1>
+      <h1>{result}</h1>
+      <input className="inputBox" onChange={inputData}></input>
+      <br/>
+      <button className="Add" onClick={doAdd}>Add</button>
     </div>
+
   );
 }
 
---
+++ src/context.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const InputCalculatorContext = createContext();
+
+const DataProvider = ({ children }) => {
+  const [data, setData] = useState(0);
+
+  return (
+    <InputCalculatorContext.Provider value={{ data, setData }}>
+      {children}
+    </InputCalculatorContext.Provider>
+  );
+};
+
+export { InputCalculatorContext, DataProvider };
